b933811cf2f2cea66bbd0b2f6e7aac4389b3c947
Chris Mears <chris.mears@gmail.com>
2020-12-31 22:31:38 -0800

    refactor: :recycle: Rename Sparkle to Spark

--- sketch.js
+++ sketch.js
@@ -10,11 +10,11 @@ const SHELLTYPES = [
   "comet",
 ];
 
-let PAUSED = true;
+let PAUSED = false;
 let MUTE = false;
 
 var shells = [];
-var sparkles = [];
+var sparks = [];
 var sounds = [];
 
 function setup() {
@@ -36,13 +36,13 @@ function draw() {
   translate(width / 2, height);
   background("rgba(0, 0, 0, 0.2)");
 
-  /* Remove the exploded shells and burnt out sparkles */
+  /* Remove the exploded shells and burnt out sparks */
   shells = shells.filter((shell) => !shell.exploded);
-  sparkles = sparkles.filter((sparkle) => sparkle.brt > 0);
+  sparks = sparks.filter((spark) => spark.brt > 0);
 
-  /* Draw the shells and sparkles */
+  /* Draw the shells and sparks */
   for (let shell of shells) shell.draw();
-  for (let sparkle of sparkles) sparkle.draw();
+  for (let spark of sparks) spark.draw();
 
   /* Generate new shell with small probability */
   if (random() < 0.03) {
@@ -65,24 +65,27 @@ function touchStarted() {
 }
 
 function keyPressed() {
+  // Space bar
   if (keyCode == 32) {
-    /* Space bar */
-    if (PAUSED) {
-      PAUSED = false;
-      /* Draw a pause symbol in top right corner */
+    if (!PAUSED) {
+      PAUSED = true;
+
+      // Draw a pause symbol in top right corner
       strokeWeight(1);
       fill(255);
       rect(width / 2 - 30, -height + 20, 10, 30);
       rect(width / 2 - 50, -height + 20, 10, 30);
+
+      // Stop the draw loop
       noLoop();
     } else {
-      PAUSED = true;
+      PAUSED = false;
       loop();
     }
     return false;
   }
+  // 's' for sound effects
   if (keyCode == 83) {
-    /* 's' for sound effects */
     MUTE = !MUTE;
   }
 }
@@ -91,10 +94,10 @@ function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
 }
 
-function Shell(pos, vel, type, sparkleTrail) {
+function Shell(pos, vel, type, sparkTrail) {
   this.pos = pos || createVector(int(random(-width / 4, width / 4)), 0);
   this.vel = vel || createVector(random(-2, 2), -random(11, 16));
-  this.sparkleTrail = sparkleTrail || random() < 0.5;
+  this.sparkTrail = sparkTrail || random() < 0.5;
   this.fuse = random(-3, -1);
   this.hue = round(random(0, 360));
   this.type = type;
@@ -107,39 +110,36 @@ function Shell(pos, vel, type, sparkleTrail) {
   }
 
   this.draw = () => {
-    // Remove burnt-out sparkles
-    // sparkles = sparkles.filter((sparkle) => sparkle.brt > 0);
-
     // Explode if at the apex (i.e., vel )
     if (this.fuse < this.vel.y) {
       this.explode();
       return;
     }
 
-    // Sparkle trail
-    if (this.sparkleTrail) {
-      // Random sparkle direction between 0 and 2 pi
-      let sparkleDir = random(0, TWO_PI);
+    // Spark trail
+    if (this.sparkTrail) {
+      // Random spark direction between 0 and 2 pi
+      let sparkDir = random(0, TWO_PI);
       // Random velocity between 0 and 1
-      let sparkleVel = random(0, 1);
+      let sparkVel = random(0, 1);
       // Random speed? - maybe the jitter?
-      let sparkleSpd = createVector(
-        sparkleVel * cos(sparkleDir),
-        sparkleVel * sin(sparkleDir)
+      let sparkSpd = createVector(
+        sparkVel * cos(sparkDir),
+        sparkVel * sin(sparkDir)
       );
-      // Sparkle position
-      let sparklePos = createVector(
-        this.pos.x + sparkleSpd.x,
-        this.pos.y + sparkleSpd.y
+      // Spark position
+      let sparkPos = createVector(
+        this.pos.x + sparkSpd.x,
+        this.pos.y + sparkSpd.y
       );
-      let s = new Sparkle(
-        sparklePos,
-        sparkleSpd,
+      let s = new Spark(
+        sparkPos,
+        sparkSpd,
         random(50, 75),
         floor(random(20, 40)),
         floor(random(0, 30))
       );
-      sparkles.push(s);
+      sparks.push(s);
     }
 
     // Color shell ±10 hue and 0-20 saturation, fixed brightness
@@ -151,8 +151,8 @@ function Shell(pos, vel, type, sparkleTrail) {
     this.vel.y = this.vel.y + GRAVITY;
   };
 
-  this.drawSparkles = (
-    numSparkles,
+  this.drawSparks = (
+    numSparks,
     velMin,
     velMax,
     fadeMin,
@@ -161,84 +161,50 @@ function Shell(pos, vel, type, sparkleTrail) {
     baseDir,
     angle
   ) => {
-    for (let i = 0; i < numSparkles; i++) {
+    for (let i = 0; i < numSparks; i++) {
       let dir = random(0, TWO_PI);
       if (baseDir != undefined) dir = baseDir + random(0, PI / angle);
       let vel = random(velMin, velMax);
-      let sparkleSpd = createVector(
+      let sparkSpd = createVector(
         this.vel.x + vel * cos(dir),
         this.vel.y + vel * sin(dir)
       );
       let hue = this.hue + round(random(-10, 10));
       let sat = round(random(0, 40));
       let fade = random(fadeMin, fadeMax);
-      let sparkle = new Sparkle(
-        this.pos.copy(),
-        sparkleSpd,
-        fade,
-        hue,
-        sat,
-        type
-      );
-      sparkles.push(sparkle);
+      let spark = new Spark(this.pos.copy(), sparkSpd, fade, hue, sat, type);
+      sparks.push(spark);
     }
   };
 
   this.explode = () => {
     if (this.type == "split") {
-      this.drawSparkles(30, 3, 5, 3, 8, "writer");
-      this.drawSparkles(10, 3, 5, 3, 6, "sparkler");
+      this.drawSparks(30, 3, 5, 3, 8, "writer");
+      this.drawSparks(10, 3, 5, 3, 6, "sparkr");
     } else if (this.type == "burst") {
-      this.drawSparkles(60, 0, 6, 3, 8, "sparkler");
+      this.drawSparks(60, 0, 6, 3, 8, "sparkr");
     } else if (this.type == "double") {
-      this.drawSparkles(90, 3, 5, 2, 4);
-      this.drawSparkles(90, 0.5, 2, 4, 6, "writer");
+      this.drawSparks(90, 3, 5, 2, 4);
+      this.drawSparks(90, 0.5, 2, 4, 6, "writer");
     } else if (this.type == "mega") {
-      this.drawSparkles(600, 0, 8, 3, 8);
+      this.drawSparks(600, 0, 8, 3, 8);
     } else if (this.type == "writer") {
-      this.drawSparkles(100, 0, 5, 1, 3, "writer");
+      this.drawSparks(100, 0, 5, 1, 3, "writer");
     } else if (this.type == "simple") {
-      this.drawSparkles(100, 0, 5, 1, 3);
+      this.drawSparks(100, 0, 5, 1, 3);
     } else if (this.type == "pent") {
       let baseDir = random(0, TWO_PI);
-      this.drawSparkles(20, 3, 5, 3, 8, "writer", baseDir + (2 / 5) * PI, 6);
-      this.drawSparkles(20, 3, 5, 3, 8, "writer", baseDir + (4 / 5) * PI, 6);
-      this.drawSparkles(20, 3, 5, 3, 8, "writer", baseDir + (6 / 5) * PI, 6);
-      this.drawSparkles(20, 3, 5, 3, 8, "writer", baseDir + (8 / 5) * PI, 6);
-      this.drawSparkles(20, 3, 5, 3, 8, "writer", baseDir + (10 / 5) * PI, 6);
+      this.drawSparks(20, 3, 5, 3, 8, "writer", baseDir + (2 / 5) * PI, 6);
+      this.drawSparks(20, 3, 5, 3, 8, "writer", baseDir + (4 / 5) * PI, 6);
+      this.drawSparks(20, 3, 5, 3, 8, "writer", baseDir + (6 / 5) * PI, 6);
+      this.drawSparks(20, 3, 5, 3, 8, "writer", baseDir + (8 / 5) * PI, 6);
+      this.drawSparks(20, 3, 5, 3, 8, "writer", baseDir + (10 / 5) * PI, 6);
     } else if (this.type == "comet") {
       let baseDir = random(0, TWO_PI);
-      this.drawSparkles(
-        10,
-        3,
-        7,
-        3,
-        8,
-        "sparkler",
-        baseDir + (2 / 3) * PI,
-        128
-      );
-      this.drawSparkles(
-        10,
-        3,
-        7,
-        3,
-        8,
-        "sparkler",
-        baseDir + (4 / 3) * PI,
-        128
-      );
-      this.drawSparkles(
-        10,
-        3,
-        7,
-        3,
-        8,
-        "sparkler",
-        baseDir + (6 / 3) * PI,
-        128
-      );
-      this.drawSparkles(200, 0, 8, 3, 8, "writer");
+      this.drawSparks(10, 3, 7, 3, 8, "sparkr", baseDir + (2 / 3) * PI, 128);
+      this.drawSparks(10, 3, 7, 3, 8, "sparkr", baseDir + (4 / 3) * PI, 128);
+      this.drawSparks(10, 3, 7, 3, 8, "sparkr", baseDir + (6 / 3) * PI, 128);
+      this.drawSparks(200, 0, 8, 3, 8, "writer");
     }
     this.exploded = true;
     if (!MUTE) {
@@ -248,22 +214,28 @@ function Shell(pos, vel, type, sparkleTrail) {
   };
 }
 
-function Sparkle(pos, vel, fade, hue, sat, type = "default") {
+function Spark(pos, vel, fade, hue, sat, type = "default") {
+  // Position
   this.pos = pos;
+
+  // Velocity
   this.vel = vel;
+
   this.fade = fade;
   this.hue = hue;
   this.sat = sat;
   this.type = type;
 
+  // Brightness
   this.brt = 255;
   this.burntime = 0;
 
   this.draw = () => {
-    // Color Sparkle
+    // Color
     stroke(this.hue, this.sat, this.brt);
 
     // Trail
+    // NB: Confusing
     let newXPos = this.pos.x + log(this.burntime) * 8 * this.vel.x;
     let newYPos =
       this.pos.y +
@@ -282,18 +254,18 @@ function Sparkle(pos, vel, fade, hue, sat, type = "default") {
       );
     }
 
-    if (this.type == "sparkler") {
+    if (this.type == "sparkr") {
       let dir = random(0, TWO_PI);
       let vel = random(0, 1);
-      let sparkleSpd = createVector(vel * cos(dir), vel * sin(dir));
-      let sparkle = new Sparkle(
-        createVector(newXPos + sparkleSpd.x, newYPos + sparkleSpd.y),
-        sparkleSpd,
+      let sparkSpd = createVector(vel * cos(dir), vel * sin(dir));
+      let spark = new Spark(
+        createVector(newXPos + sparkSpd.x, newYPos + sparkSpd.y),
+        sparkSpd,
         random(50, 75),
         round(random(20, 40)),
         round(random(0, 30))
       );
-      sparkles.push(sparkle);
+      sparks.push(spark);
     }
 
     // Fade
